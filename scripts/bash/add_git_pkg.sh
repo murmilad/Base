561acf2c6ee033d663563cb690f9834db3326c97
Alexey Kosarev <alex@homyaki.info>
2012-08-20 15:32:50 +0400

    Add git repo script

--- scripts/bash/add_git_pkg.sh
+++ scripts/bash/add_git_pkg.sh
@@ -7,3 +7,5 @@ git clone --bare $1 /opt/git/$1.git
 rm -rf $1
 sudo mv /opt/git/$1.git/hooks/post-update.sample /opt/git/$1.git/hooks/post-update; sudo chmod a+x /opt/git/$1.git/hooks/post-update;
 cd /opt/git/$1.git/; sudo git update-server-info;
+cd /home/alex/git
+git clone /opt/git/$1.git
