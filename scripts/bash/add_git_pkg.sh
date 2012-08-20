479e217927be615d85cdae27ce1a0dcd269a1384
Alexey Kosarev <alex@homyaki.info>
2012-08-20 15:26:59 +0400

    Add git repo script

--- scripts/bash/add_git_pkg.sh
+++ scripts/bash/add_git_pkg.sh
@@ -1,6 +1,8 @@
+cd $1
 git init
 git add .
-git commit -m $2
+git commit -m "$2"
+cd ..
 git clone --bare $1 /opt/git/$1.git
 rm -rf $1
 sudo mv /opt/git/$1.git/hooks/post-update.sample /opt/git/$1.git/hooks/post-update; sudo chmod a+x /opt/git/$1.git/hooks/post-update;
