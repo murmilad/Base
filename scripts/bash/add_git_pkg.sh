24608f5aa23b3590f40ff15dde0a4cfca0ad9d0f
Alexey Kosarev <alex@homyaki.info>
2012-09-11 22:34:01 +0400

    Make git make repo safeer

--- scripts/bash/add_git_pkg.sh
+++ scripts/bash/add_git_pkg.sh
@@ -4,7 +4,7 @@ git add .
 git commit -m "$2"
 cd ..
 git clone --bare $1 /opt/git/$1.git
-rm -rf $1
+mv $1 $1.back
 sudo mv /opt/git/$1.git/hooks/post-update.sample /opt/git/$1.git/hooks/post-update; sudo chmod a+x /opt/git/$1.git/hooks/post-update;
 cd /opt/git/$1.git/; sudo git update-server-info;
 cd /home/alex/git
